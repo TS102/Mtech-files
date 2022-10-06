a7631e66cc8bfcf621ee94198661799ac836d80f
Tyler Sun <tylersun10@gmail.com>
2022-10-06 09:39:40 -0600

    finsihed text validtion app

--- TextValidation/TextValidation/ViewController.swift
+++ TextValidation/TextValidation/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet var passWordTextField: UITextField!
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var alertLabel: UILabel!
+    @IBOutlet var passWordLabel: UILabel!
+    
     let specialCharacters = "!~`@#$%^&*-+();:={}[],.<>?\\/\"\'"
     
     override func viewDidLoad() {
@@ -19,34 +21,71 @@ class ViewController: UIViewController {
     }
     //try the black diamond part after checking off
     
-    
-    
     @IBAction func loginButton(_ sender: Any) {
         let characterset = CharacterSet(charactersIn: specialCharacters)
-    
-        if passWordTextField.text?.rangeOfCharacter(from: characterset) != nil && passWordTextField.text!.count >= 8 {
-            alertLabel.text = "Correct Password"
-        } else {
-            alertLabel.text = "Please enter password"
-        }
-     
-        //        if (passWordTextField.text?)! = lowercase {
-        //            alertLabel.text = "works"
-        //        }
-        //        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        //
-        //        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        //        if userNameTextField == emailPred {
-        //        print("works")
-        //        }
-        //    }
-        
-    }
-}
 
-func containLowerCase (_ value: String) -> Bool {
-    let regularExpression = ".*[a-z]+.*"
-    let predicate = NSPredicate(format: "Self Matches %@", regularExpression)
-    return !predicate.evaluate(with: value)
-    
+        if let password = passWordTextField.text {
+            if let incorrectPassword = invalidPassword(password) {
+                passWordLabel.text = incorrectPassword
+            }
+        }
+        func invalidPassword(_ value: String) -> String? {
+            if value.count < 8 {
+                return "Needs more characters!"
+            }
+            if !lowerCase(value) {
+                return "Needs a lowercase!"
+            } else {
+                passWordLabel.text = ""
+            }
+
+            if !upperCase(value) {
+                return "Needs a Uppercase!"
+            } else {
+                passWordLabel.text = ""
+            }
+
+            if passWordTextField.text?.rangeOfCharacter(from: characterset) == nil {
+                return "Needs a symbol!"
+            }
+            return ""
+        }
+        func containsSymbols(_ value: String) -> Bool {
+            let regularExpression = "^[A-Za-z0-9 !\"#$%&'()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~].{8,}$"
+            let predicate = NSPredicate(format: "Self Matches %@", regularExpression)
+            return predicate.evaluate(with: value)
+        }
+
+        func lowerCase(_ value: String) -> Bool {
+            let regularExpression = ".*[a-z]+.*"
+            let predicate = NSPredicate(format: "Self Matches %@", regularExpression)
+            return predicate.evaluate(with: value)
+        }
+
+        func upperCase(_ value: String) -> Bool {
+            let regularExpression = ".*[A-Z]+.*"
+            let predicate = NSPredicate(format: "Self Matches %@", regularExpression)
+            return predicate.evaluate(with: value)
+        }
+        
+        if let userName = userNameTextField.text {
+            if let invalidUser = invalidUser(userName) {
+                alertLabel.text = invalidUser
+            }
+            func invalidUser(_ value: String) -> String? {
+                if validEmail(value) {
+                    alertLabel.textColor = .systemBlue
+                    return "Login successful!"
+                }
+                return "Invalid Email"
+            }
+            
+            func validEmail(_ value: String) -> Bool {
+                let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+                let predicate = NSPredicate(format: "Self Matches %@", regularExpression)
+                return predicate.evaluate(with: value)
+            }
+            
+        }
+    }
 }
